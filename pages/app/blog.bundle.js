0fa1a3473ce55e492cff71a37f2ea76fb1ef8a68
Yuliya Volkova <Yuliyavolkova@gmail.com>
2018-03-20 11:00:41 +0300

    verstka_4_pages_reload

--- pages/app/blog.bundle.js
+++ pages/app/blog.bundle.js
@@ -1,1 +1,2 @@
 webpackJsonp([3],{28:function(n,o,t){n.exports=t(29)},29:function(n,o,t){"use strict";t(0),t(30);var e=t(1),u=function(n){return n&&n.__esModule?n:{default:n}}(e);console.log("blog.js"),(0,u.default)()},30:function(n,o){}},[28]);
+//# sourceMappingURL=blog.bundle.js.map
